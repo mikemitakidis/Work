6e0fd46753164111883800ae99d4ccf4cd763f4c
mikemitakidis <75459710+mikemitakidis@users.noreply.github.com>
2024-12-12 21:24:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -78,4 +78,10 @@ def upload_file():
 if __name__ == '__main__':
     if not os.path.exists("uploads"):
         os.makedirs("uploads")
-    app.run(debug=True)
+        
+        import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
